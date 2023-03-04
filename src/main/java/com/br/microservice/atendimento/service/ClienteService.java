4055f49733a526395f6ff57200cb8490f300d089
BrunaMassuchini <brunaccelestino@gmail.com>
2023-03-04 16:37:51 -0300

    sonar

--- src/main/java/com/br/microservice/atendimento/service/ClienteService.java
+++ src/main/java/com/br/microservice/atendimento/service/ClienteService.java
@@ -18,7 +18,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.List;
